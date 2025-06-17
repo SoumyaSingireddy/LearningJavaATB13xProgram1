6aef7d52074ae6c209a9317773cab1447e5bc4e3
SoumyaSingireddy <sgsoumya1990@gmail.com>
2025-06-17 15:15:22 +0530

    17th June

---
+++ src/ex_03_Literals/Lab021_Char_Literal.java
@@ -0,0 +1,21 @@
+package ex_03_Literals;
+
+public class Lab021_Char_Literal {
+    public static void main(String[] args) {
+        char c1 = 'A' ;
+        char c2 = 'B' ;
+
+
+        //Escape char
+        char new_line = '\n' ;
+        char tab_line = '\t' ;
+        char back_space = '\b' ;
+        char carriage_return = '\r' ;
+
+        System.out.println("SoumyaReddy");
+        System.out.println("Soumya"+new_line+"Reddy");
+        System.out.println("Soumya"+tab_line+"Reddy");
+        System.out.println("Soumya"+back_space+"Reddy");
+        System.out.println("Soumya is young"+carriage_return+"Reddy");
+    }
+}
