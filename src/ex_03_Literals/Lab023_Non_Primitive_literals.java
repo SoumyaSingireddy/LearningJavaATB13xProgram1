5baaaa8bbd2e7870f61b92440dfa210ceaff67b9
SoumyaSingireddy <sgsoumya1990@gmail.com>
2025-06-18 11:50:31 +0530

    17th June

---
+++ src/ex_03_Literals/Lab023_Non_Primitive_literals.java
@@ -0,0 +1,21 @@
+package ex_03_Literals;
+
+public class Lab023_Non_Primitive_literals {
+    public static void main(String[] args) {
+        //Primitive Data Types-Defined by Java Guys
+        // // max,Min,Size defined
+        byte b=10; // 1Byte -> 8 Bits
+        int age=65; // 4 Byte->32 Bits
+
+        // Non Primitive (Defined by users).Reference data Types
+        // No Size,max,min
+        //Byte - 8 ,Bits - 64
+        String name = "Pramod"; // String is bunch of char.
+        int[] arrays_of_items = new int[10];
+        //String,Arrays,Classes,enum..
+
+        String s = null;
+        //int age = null; integer cannot be null
+        //
+    }
+}
