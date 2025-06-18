a07029c10c515b7cd049290399878f3ea52267b5
SoumyaSingireddy <sgsoumya1990@gmail.com>
2025-06-18 15:48:20 +0530

    17th June

---
+++ src/ex_06_Ternary_Operator/Lab044_TO.java
@@ -0,0 +1,10 @@
+package ex_06_Ternary_Operator;
+
+public class Lab044_TO {
+    public static void main(String[] args) {
+        // Age to Vote?
+        int age = 10;
+        String canIVote = age > 18 ? "Yes" : "No";
+        System.out.println(canIVote);
+    }
+}
