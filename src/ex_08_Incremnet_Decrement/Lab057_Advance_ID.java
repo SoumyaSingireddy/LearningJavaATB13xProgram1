db12ee015bde570f4aa136e17d3f90460762b996
SoumyaSingireddy <sgsoumya1990@gmail.com>
2025-06-19 13:48:48 +0530

    19th June

---
+++ src/ex_08_Incremnet_Decrement/Lab057_Advance_ID.java
@@ -0,0 +1,10 @@
+package ex_08_Incremnet_Decrement;
+
+public class Lab057_Advance_ID {
+    public static void main(String[] args) {
+
+        int a =10;
+        System.out.println(a++ + a);
+
+    }
+}
---
+++ src/ex_08_Incremnet_Decrement/Lab058_Exp1.java
@@ -0,0 +1,9 @@
+package ex_08_Incremnet_Decrement;
+
+public class Lab058_Exp1 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a++ + ++a);
+        System.out.println(a);
+    }
+}
