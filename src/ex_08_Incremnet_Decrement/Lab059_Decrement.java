1195b6ef3dedf40002d03439192a699cbdb9ed3e
SoumyaSingireddy <sgsoumya1990@gmail.com>
2025-06-19 13:54:28 +0530

    19th June

---
+++ src/ex_08_Incremnet_Decrement/Lab059_Decrement.java
@@ -0,0 +1,10 @@
+package ex_08_Incremnet_Decrement;
+
+public class Lab059_Decrement {
+    public static void main(String[] args) {
+        int a = 10;
+        int result_post = a--;
+        System.out.println(result_post);
+        System.out.println(a);
+    }
+}
---
+++ src/ex_08_Incremnet_Decrement/Lab060_Dec2.java
@@ -0,0 +1,11 @@
+package ex_08_Incremnet_Decrement;
+
+public class Lab060_Dec2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int result = --a;
+        System.out.println(result);
+        System.out.println(a);
+    }
+
+}
