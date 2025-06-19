8c95b98fec3c2c52fc3a148beb8c1968a40d4abb
SoumyaSingireddy <sgsoumya1990@gmail.com>
2025-06-19 11:47:58 +0530

    19th June

---
+++ src/ex_07_UserInput/Lab052_UserInput_CL2.java
@@ -0,0 +1,10 @@
+package ex_07_UserInput;
+
+public class Lab052_UserInput_CL2 {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+
+    }
+}
