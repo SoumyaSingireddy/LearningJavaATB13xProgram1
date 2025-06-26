d6547f511ca623d6ed4cc88e3c3c9a8930322fbd
SoumyaSingireddy <sgsoumya1990@gmail.com>
2025-06-26 12:48:43 +0530

    25th june

---
+++ src/Ex_13_DoWhile/Lab084_Diff_While_DoWhile.java
@@ -0,0 +1,12 @@
+package Ex_13_DoWhile;
+
+public class Lab084_Diff_While_DoWhile {
+    public static void main(String[] args) {
+        int a = 0;
+        do{
+            System.out.println(a);
+            a++;
+        }
+        while (a<0);
+    }
+}
---
+++ src/Ex_13_DoWhile/Lab087_Char_IsVowel.java
@@ -0,0 +1,17 @@
+package Ex_13_DoWhile;
+
+import java.util.Scanner;
+
+public class Lab087_Char_IsVowel {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println("Enter a single alphabet: ");
+        char ch = Scanner.next().toLowerCase().charAt(0);
+
+        switch(ch){
+            case 'a' , 'e', 'i' , 'o' , 'u' -> System.out.println("Vowel");
+            default-> System.out.println("Consonents");
+        }
+
+    }
+}
