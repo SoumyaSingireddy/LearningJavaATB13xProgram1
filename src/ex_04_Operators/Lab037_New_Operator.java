413e950f816649c81a34257d60777b27e2b753bb
SoumyaSingireddy <sgsoumya1990@gmail.com>
2025-06-26 12:48:51 +0530

    25th june

--- src/ex_04_Operators/Lab037_New_Operator.java
+++
@@ -1,8 +0,0 @@
-package ex_04_Operators;
-
-public class Lab037_New_Operator {
-    public static void main(String[] args) {
-        //New Operator - oops
-        String name = new String(original: "Soumya");
-    }
-}
--- src/ex_04_Operators/Lab038_InstanceOf_Operator.java
+++
@@ -1,8 +0,0 @@
-package ex_04_Operators;
-
-public class Lab038_InstanceOf_Operator {
-    public static void main(String[] args) {
-        String s1 = new String(original: "Soumya");
-        System.out.println(s1 instanceof String);
-    }
-}
